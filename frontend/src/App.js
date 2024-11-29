b69932284ab43c10bae05a7b2ca945037cea7eca
mysticblackmask <black.mask0209@outlook.com>
2024-11-28 18:19:08 -0800

    divide route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,26 @@ import React from 'react';
 import store from './store';
 import { Provider } from 'react-redux';
 import ProductOffers from './pages/ProductOffers';
+import OfferDetail from './pages/ProductOffers/OfferDetail';
 import Header from './components/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.scss';
 
 function App() {
   return (
     <Provider store={store}>
       <Header />
-      <ProductOffers />
+      <Router
+        future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true,
+        }}
+      >
+        <Routes>
+          <Route path="/" element={<ProductOffers />} />
+          <Route path="/offer/:id" element={<OfferDetail />} />
+        </Routes>
+      </Router>
     </Provider>
   );
 }
